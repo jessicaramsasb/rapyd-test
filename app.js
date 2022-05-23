72ed491275ccd1ba0ab8d78dd55da75a1637f6c0
Jéssica Ramírez <jramirez@sherpabrokers.com>
2022-05-23 16:37:46 -0500

    Feat: Add new endpoint to retrieve payment required fields

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.listen(3000)
 
 app.get('/country', async (req, res) => {
   try {
-    const result = await makeRequest('GET', '/v1/payment_methods/country?country=mx')
+    const country = req.query.country ? req.query.country : null
+    const result = await makeRequest('GET', `/v1/payment_methods/country?country=${country}`)
 
     res.json(result)
   } catch (error) {
@@ -15,3 +16,16 @@ app.get('/country', async (req, res) => {
   }
 })
 
+app.get('/payment-required-fields', async (req, res) => {
+  try {
+    const paymentMethod = req.query.country ? req.query.paymentMethod : null
+
+    if (paymentMethod === null) res.status(400).json({ status: 400, message: "Payment method is required" })
+
+    const result = await makeRequest('GET', `/v1/payment_methods/required_fields/${paymentMethod}`)
+
+    res.json(result)
+  } catch (error) {
+    console.error('Error completing request', error)
+  }
+})
